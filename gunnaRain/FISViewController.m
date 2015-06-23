89e502bd5d2e298e7bbf5fcb503902523c92cc0d
yoyoyoseob <yoyoyoseob@gmail.com>
2015-06-23 09:23:31 -0400

    tests passing and uilabel color

--- gunnaRain/FISViewController.m
+++ gunnaRain/FISViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "FISViewController.h"
+#import <Forecastr.h>
 
 @interface FISViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *weatherLabel;
+
 
 @end
 
@@ -18,6 +21,36 @@
 {
     [super viewDidLoad];
 
+    
+    Forecastr *forecastClient = [Forecastr sharedManager];
+    forecastClient.apiKey = @"738a9332915dd0d25b3ea15f5ba877d6";
+    
+    
+    // Portland: 45.200 | 122.6819
+    // Mumbai: 18.9750 | 72.8258
+    
+    // NSLog(@"Getting the weather now");
+    [forecastClient getForecastForLatitude:18.9750 longitude:72.8258 time:nil exclusions:nil extend:nil success:^(id JSON) {
+        // NSLog(@"Got the weather!");
+        if ([JSON[@"currently"][@"precipProbability"] integerValue] == 1){
+            
+            self.weatherLabel.text = @"Yep";
+            self.weatherLabel.textColor = [UIColor redColor];
+        }
+        else if ([JSON[@"currently"][@"precipProbability"] integerValue] == .5){
+            self.weatherLabel.text = @"It might rain";
+            self.weatherLabel.textColor = [UIColor blackColor];
+        }
+        else {
+            NSLog(@"%@", JSON[@"currently"][@"precipProbability"]);
+            self.weatherLabel.text = @"Nope";
+            self.weatherLabel.textColor = [UIColor greenColor];
+        }
+        
+    } failure:^(NSError *error, id response) {
+        NSLog(@"There was an issue getting the forecast");
+    }];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
